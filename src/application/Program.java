2f0e73085adcf769f91cac45e7959447451b62d0
Leandro Machado <leandrovictoram@gmail.com>
2024-12-28 16:20:53 -0300

    Programinha de estoque

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Triangle;
+import entities.Product;
 
 public class Program {
 
@@ -35,6 +36,37 @@ public class Program {
 		} else {
 			System.out.println("Larger area: Y");			
 		}
+		
+		// ------------------------------------------------
+		
+		Product product = new Product();
+		
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println("Enter the number of products to be remove from stock: ");
+		quantity = sc.nextInt();
+		product.removeProduct(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
 		
 		sc.close();
 	}
