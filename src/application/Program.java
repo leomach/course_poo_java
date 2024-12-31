b89d3e0195275d83a5346c07ad3fb0b6cc173b2a
Leandro Machado <leandrovictoram@gmail.com>
2024-12-31 10:18:38 -0300

    Vetores - Stack e Heap - Alocação de memória em referência e primitivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -66,6 +66,25 @@ public class Program {
 		System.out.println();
 		System.out.println("Updated data: " + product);
 		
+		// ------------------------------------------
+		
+		int n = sc.nextInt();
+		Product[] vect = new Product[n];
+		
+		for (int i=0; i<vect.length; i++) {
+			sc.nextInt();
+			name = sc.nextLine();
+			price = sc.nextDouble();
+			vect[i] = new Product(name, price);
+		}
+		
+		double sum = 0.0;
+		for (int i=0; i<vect.length; i++) {
+			sum += vect[i].getPrice();
+		}
+		
+		double avg = sum / vect.length;
+		System.out.printf("Average price = %.2f%n", avg);
 		
 		sc.close();
 	}
